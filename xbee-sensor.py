886d391b71685aae7f0f2ca36738c3d1b9d977b4
Aaron Huslage <huslage@gmail.com>
2012-10-30 20:44:54 -0400

    initial

---
+++ xbee-sensor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from xbee import ZigBee
+import serial
+import csv
+
+ser = serial.Serial('/dev/tty.usbserial-A900XSAC', 9600)
+
+xbee = ZigBee(ser)
+
+# Continuously read and print packets
+while True:
+    try:
+        response = xbee.wait_read_frame()
+        source_addr_long = response['source_addr_long'].encode("hex")
+	with open('sensorlog.csv', 'a') as f:
+            csvout = csv.writer(f)
+	    for sample in response['samples']:
+	        row = [source_addr_long, sample['adc-0'], sample['adc-1']]
+		csvout.writerow(row)
+    except KeyboardInterrupt:
+        break
+        
+ser.close()
